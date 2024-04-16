0c4d9f903ac1ef0661337c536289d91e0f5653c3
Kerpooo <kevinrincon8@gmail.com>
2024-04-16 01:40:55 -0500

    fix(auth): :pencil2:

--- src/app/(unauthenticated)/signin/page.tsx
+++ src/app/(unauthenticated)/signin/page.tsx
@@ -8,6 +8,7 @@ export default async function SignIn() {
     return (
         <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
             <div className="hidden lg:block">
+                {/* TODO: Poner la imagen correspondiente */}
                 <img
                     alt="Image"
                     className="object-cover w-full h-full aspect-none lg:aspect-w-1 lg:aspect-h-1"
@@ -19,7 +20,9 @@ export default async function SignIn() {
             <div className="flex items-center justify-center py-12">
                 <div className="mx-auto w-[350px] space-y-6">
                     <div className="space-y-2 text-center">
-                        <h1 className="text-3xl font-bold">Login</h1>
+
+                        {/* TODO: Poner el logo de kidsplace */}
+                        <h1 className="text-3xl font-bold">Sign In</h1>
                         <p className="text-gray-500 dark:text-gray-400">Ingresa con tu cuenta de google</p>
                     </div>
                     <div className="space-y-4">
