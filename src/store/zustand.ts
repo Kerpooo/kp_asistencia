fa70620c71c866cb022b8dce49bf1674cb661f6e
Kerpooo <kevinrincon8@gmail.com>
2024-05-17 12:47:42 -0500

    fix: :bug: Hora establecida a 00:00:00 para el estado global

--- src/store/zustand.ts
+++ src/store/zustand.ts
@@ -7,24 +7,31 @@ type FechaStore = {
 }
 
 
-export const useFechaStore = create<FechaStore>((set) => ({
-    fechaSeleccionada: new Date(),
-    setFechaSeleccionada: (fecha: Date) => set({ fechaSeleccionada: fecha }),
-}));
+const setToStartOfDay = (date: Date): Date => {
+    const newDate = new Date(date)
+    newDate.setHours(0, 0, 0, 0)
+    return newDate
+};
 
+
+
+export const useFechaStore = create<FechaStore>((set) => ({
+    fechaSeleccionada: setToStartOfDay(new Date()),
+    setFechaSeleccionada: (fecha: Date) => set({ fechaSeleccionada: fecha }),
+}))
 
 
 
 // TODO: ENVIAR EL CURSO PARA FILTRAR ALUMNOS POR CURSO
 
 type CursoStore = {
-    cursoSeleccionado: string | undefined
-    setCursoSeleccionado: (curso: string | undefined) => void
+    cursoSeleccionado: string
+    setCursoSeleccionado: (curso: string) => void
 }
 
 export const useCursoStore = create<CursoStore>((set) => ({
     cursoSeleccionado: "",
-    setCursoSeleccionado: (curso: string | undefined) => set({ cursoSeleccionado: curso })
+    setCursoSeleccionado: (curso: string) => set({ cursoSeleccionado: curso })
 
 }))
 
