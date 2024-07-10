ee7105b35c52a912e49d5f27e21bfb66c251395a
Kerpooo <kevinrincon8@gmail.com>
2024-07-09 20:11:58 -0500

    style: :lipstick: Correccion y simplificacion de los estilos

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -10,7 +10,7 @@ export default async function AuthLayout({
 }) {
 
     return (
-        <div className="flex min-h-screen w-full flex-col bg-muted/40" >
+        <div className="h-screen w-full  bg-muted/40" >
             <SideNavBar />
             <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
                 <NavBar />
