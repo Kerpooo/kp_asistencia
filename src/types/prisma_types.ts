9e511fc385ab3d56d14fc1e3b7f67ee682a5f5bc
Kerpooo <kevinrincon8@gmail.com>
2024-07-09 20:33:22 -0500

    chore: :label: Nuvos tipos para interfaces, funciones, etc

--- src/types/prisma_types.ts
+++ src/types/prisma_types.ts
@@ -1,18 +1,22 @@
 import { type obtenerAsistenciaAlumnos } from "@/server/actions/asistencia"
-import { type listarCursos, type obtenerCurso, type listarEstudiantesCurso } from "@/server/actions/curso"
-import { type listarKids } from "@/server/actions/kid"
+import type { listarCursos, obtenerCurso } from "@/server/actions/curso"
+import type { listarEstudiantes, obtenerEstudiantesCurso, listarKids } from "@/server/actions/kid"
 import { type Prisma } from "@prisma/client"
+import { type ArrayElement } from "./type"
 
 export type AsistenciaType = Prisma.PromiseReturnType<typeof obtenerAsistenciaAlumnos>
-
-
 
 //Cursos
 export type ListaCursosType = Prisma.PromiseReturnType<typeof listarCursos>
 export type CursoType = Prisma.PromiseReturnType<typeof obtenerCurso>
 
+
+export type CursoCardType = ArrayElement<NonNullable<ListaCursosType>>
 //Kids
 export type ListarKidsType = Prisma.PromiseReturnType<typeof listarKids>
 
 //Lista Alumnos
-export type ListaAlumnosCurso = Prisma.PromiseReturnType<typeof listarEstudiantesCurso>
+export type ListaAlumnosCurso = Prisma.PromiseReturnType<typeof obtenerEstudiantesCurso>
+export type ListaAlumnos = Prisma.PromiseReturnType<typeof listarEstudiantes>
+export type AlumnoCurso = ArrayElement<NonNullable<ListaAlumnosCurso>>
+export type Alumnos = ArrayElement<NonNullable<ListaAlumnos>>
